b4fa356327b20fe28175708481d141d02fa8f587
Jon Friskics <jon-friskics@pluralsight.com>
2020-12-01 00:21:54 -0500

    day 1 solution

--- day_01.py
+++ day_01.py
@@ -3,8 +3,15 @@ import os
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 file = open(dir_path + '/inputs/day_01.txt', 'r')
-input = file.read().split('\n')
-# input = file.read().strip()
+input = file.read().strip().split('\n')
 
-sum_star01 = 0
-sum_star02 = 0
+star1_input = [int(i) for i in input]
+star1_input.sort()
+
+for i in range(len(star1_input)):
+  for j in range(len(star1_input)-i):
+    if(star1_input[i] + star1_input[j] == 2020):
+      print("star 1 ", star1_input[i] * star1_input[j])
+    for z in range(len(star1_input)-j):
+      if(star1_input[i] + star1_input[j] + star1_input[z] == 2020):
+        print("star 2 ", star1_input[i] * star1_input[j] * star1_input[z])
